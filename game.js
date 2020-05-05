9eae071a4e82463b0fe2a9da9a78e8133379e75b
Dblackone <vollmannakarakiri0@gmail.com>
2020-05-05 15:03:12 +0100

    create score js

--- game.js
+++ game.js
@@ -58,5 +58,87 @@ let questions = [
       option4: "Wakizashi",
       answer: 2,
     },
-  ];
+];
+const Correct_Point = 2;
+const Max_Questions = 5;
+
+startQuiz = () => {
+  questionCounter = 0;
+  sscore = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= Max_Questions) {
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("end.html");
+  }
+
+  questionCounter++;
+  questionCounterText.innerText = `${questionCounter} of ${Max_Questions}`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  options.forEach((option) => {
+    const number = option.dataset["number"];
+    option.innerText = currentQuestion["option" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+options.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedOption = e.target;
+    const selectedAnswer = selectedOption.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      increaseScore(Correct_Point);
+      selectedOption.parentElement.classList.add(classToApply);
+    } else {
+      selectedOption.parentElement.classList.add(classToApply);
+
+      if (currentQuestion.answer === 1) {
+        quizOption1.classList.add("correct");
+      } else if (currentQuestion.answer === 2) {
+        quizOption2.classList.add("correct");
+      } else if (currentQuestion.answer === 3) {
+        quizOption3.classList.add("correct");
+      } else if (currentQuestion.answer === 4) {
+        quizOption4.classList.add("correct");
+      }
+    }
+
+    setTimeout(() => {
+      quizOption1.classList.remove("correct");
+      quizOption2.classList.remove("correct");
+      quizOption3.classList.remove("correct");
+      quizOption4.classList.remove("correct");
+      selectedOption.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 650);
+  });
+});
+
+nextButton.addEventListener("click", (event) => {
+  getNewQuestion();
+});
+
+increaseScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startQuiz();
   
---
+++ score.js
@@ -0,0 +1,6 @@
+
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
+
